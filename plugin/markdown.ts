c13abc5a8e078744f1b7132c2f19d59ef94a6f4f
ankys <ankysp@gmail.com>
2023-03-28 14:18:59 +0900

    use marked in markdown plugin

--- plugin/markdown.ts
+++ plugin/markdown.ts
@@ -1,6 +1,8 @@
 
-import * as Markdown from "https://deno.land/x/markdown@v2.0.0/mod.ts";
+// import * as Markdown from "https://deno.land/x/markdown@v2.0.0/mod.ts";
+import * as Marked from "https://esm.sh/marked@4.2.12";
 
 export async function convert(text: string): Promise<string> {
-	return Markdown.Marked.parse(text).content;
+	// return Markdown.Marked.parse(text).content;
+	return Marked.marked.parse(text);
 }
